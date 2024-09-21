db091e2ad457a22afca5dac719ddf3d482136fc0
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-09-21 23:23:57 +0300

    Create rus.ts

---
+++ rus.ts
@@ -0,0 +1,21 @@
+function bns(arr: number[], target:number): number{
+  let left =0;
+  let right = arr.length - 1;
+
+
+  while(left <= right){
+    const mid = Math.floor((left+right)/2);
+
+    if(arr[mid] ===target){
+      return mid;
+    }
+
+    if(arr[mid]< target){
+      left = mid + 1;
+    } else{
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+}
